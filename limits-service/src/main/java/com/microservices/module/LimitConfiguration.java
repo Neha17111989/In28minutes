d4bb42b675b6eca56f461e9d81bf510bfaf29bef
nmehta6812 <nmehta6812@AIPL-PUN-LT129.altimetrik.com>
2020-01-03 17:16:09 +0530

    second MS

---
+++ limits-service/src/main/java/com/microservices/module/LimitConfiguration.java
@@ -0,0 +1,32 @@
+package com.microservices.module;
+
+public class LimitConfiguration {
+
+	private int maximum;
+	private int minimum;
+	
+	LimitConfiguration(){}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+
+}
