19766e0b1b5a279084b303469af1eadc43f3cf9c
nmehta6812 <nmehta6812@AIPL-PUN-LT129.altimetrik.com>
2020-01-06 15:10:55 +0530

    cloud sleuth+zuul integration+ request ID

--- currency-exchange-service/src/main/java/com/microservices/CurrencyExcahngeController.java
+++ currency-exchange-service/src/main/java/com/microservices/CurrencyExcahngeController.java
@@ -1,5 +1,7 @@
 package com.microservices;
 
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +17,13 @@ public class CurrencyExcahngeController {
 	@Autowired
 	CurrencyRepo repo;
 	
+	Logger logg=(Logger) LoggerFactory.getLogger(this.getClass());
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExcangeValue retrieveExchangeValue(@PathVariable String from,@PathVariable String to) {
 		//ExcangeValue obj=new ExcangeValue(1000, from, to, 65);
 		ExcangeValue obj=repo.findByFromAndTo(from, to);
+		logg.info("{}",obj);
 		obj.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		return obj;
 	}
